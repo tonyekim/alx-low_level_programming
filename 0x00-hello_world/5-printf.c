485df1ed2b39c8c93581231e84f24f6bb6f9b45f
tonyekim <otoobongekim@gmail.com>
2022-06-02 14:03:34 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
+ * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
