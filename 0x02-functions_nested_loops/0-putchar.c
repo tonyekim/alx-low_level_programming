7fb53f55d34382541b2d649bcf942c1d1d55f7bf
tonyekim <otoobongekim@gmail.com>
2022-06-07 11:51:25 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,25 @@
-#include "main.h"
-/**
- * main - Entry point
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
  *
- * Return: Always 0 (Success)
+ */
+
+#include "putchar.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
