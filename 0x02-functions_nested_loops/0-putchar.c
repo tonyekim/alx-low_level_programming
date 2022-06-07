bcbadca7aa1de119ca1c87d6d2e3babf57431863
tonyekim <otoobongekim@gmail.com>
2022-06-07 12:23:18 -0700

    loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,17 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
-
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 8; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
 	return (0);
 }
