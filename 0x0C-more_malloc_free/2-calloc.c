707142d6bb5b26920c6c5225e018839c90950600
tonyekim <otoobongekim@gmail.com>
2022-06-30 14:25:59 -0700

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,21 +9,21 @@
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
 	void *ptr;
-	unsigned int i; 
+	unsigned int i; /* match unsigned arguments */
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb <= 0 || size <= 0) /* validate input */
 		return (NULL);
 
-	ptr = malloc (nmemb * size);
+	/* allocate memory and check if error */
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 
+	/* set allocated memory values to 0 */
 	for (i = 0; i < nmemb * size; i++)
-		*((char *)ptr + i) = 0;
+		*((char *)ptr + i) = 0; /* type cast assigning values*/
 
 	return (ptr);
 }
-
